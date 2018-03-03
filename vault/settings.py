11597d95d70280816e10ae97e8e70704b24f0a62
Michael Helvey <michael.helvey1@gmail.com>
2018-03-02 18:27:47 -0800

    chore: add django_heroku

--- vault/settings.py
+++ vault/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -144,3 +145,5 @@ DATABASES = {
 }
 DATABASES['default'].update(dj_database_url.config(
     conn_max_age=500, ssl_require=-True))
+
+django_heroku.settings(locals())
